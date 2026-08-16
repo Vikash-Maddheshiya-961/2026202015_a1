acf7b68605a373ad43c91dcf0cbe55aada4efa8e
Vikash Maddheshiya <vikash.maddheshiya@students.iiit.ac.in>
2026-08-16 17:49:37 +0530

    Added password input to login function

--- login.cpp
+++ login.cpp
@@ -8,6 +8,21 @@ void login_user(){
     cout << "Welcome" << username <<"!" << endl;
     return;
 }
+
+void showLogin() {
+    string username;
+    string password;
+
+    cout << "Enter username: ";
+    cin >> username;
+
+    cout << "Enter password: ";
+    cin >> password;
+    
+    cout << "Login attempt for " << username << endl;
+      
+    return;
+}
 int main(){
     
     return 0;
