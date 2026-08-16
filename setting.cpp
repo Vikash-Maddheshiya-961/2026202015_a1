ef4aa205c5e053418d98647804bc5541335110d8
Vikash Maddheshiya <vikash.maddheshiya@students.iiit.ac.in>
2026-08-16 18:31:48 +0530

    Created branch Settings and Added basic settings display into setting.cpp file

---
+++ setting.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void showSettings(){
+    cout << "===== Settings =====" << endl;
+    return ;
+}
+
+int main(){
+    showSettings();
+    return 0;
+}
