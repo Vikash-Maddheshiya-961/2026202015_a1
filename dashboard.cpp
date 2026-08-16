2322a99dffc142987a7aee5fce88b033636ff509
Vikash Maddheshiya <vikash.maddheshiya@students.iiit.ac.in>
2026-08-16 18:21:07 +0530

    Added navigation options to dashboard!

--- dashboard.cpp
+++ dashboard.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 void showDashboard(){
     cout << "Student Dashboard!" << endl;
+    cout << "1. Profile" << endl;
+    cout << "2. Courses" << endl;
+    cout << "3. Settings" << endl;
     return;
 }
 
