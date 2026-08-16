2f751dae9757233f3fa3b3d4c622bda5adaae763
Vikash Maddheshiya <vikash.maddheshiya@students.iiit.ac.in>
2026-08-16 18:07:07 +0530

    Created new Branch Profile and basic student profile cpp file and some implementation!

---
+++ profile.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void showProfile(){
+    string name = "Vikash Maddheshiya";
+    cout << "Student Name: " << name << endl;
+    return;
+}
+
+int main(){
+  showProfile();
+  return 0;
+}
