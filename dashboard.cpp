8f704eb80e88b505e7ee9a42668cbccb7a2779fc
Vikash Maddheshiya <vikash.maddheshiya@students.iiit.ac.in>
2026-08-16 18:19:25 +0530

    Created branch dashboard and Added basic dashboard display in dashboard.cpp

---
+++ dashboard.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void showDashboard(){
+    cout << "Student Dashboard!" << endl;
+    return;
+}
+
+int main(){
+    showDashboard();
+    return 0;
+}
