7dfc5b8ea23e10f52f9771c43887d95e5054ad2a
Vikash Maddheshiya <vikash.maddheshiya@students.iiit.ac.in>
2026-08-16 19:43:55 +0530

    Added profile heading in profile.cpp in wrong_place branch

--- profile.cpp
+++ profile.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 
 void showProfile(){
+
     string name = "Vikash Maddheshiya";
     string rollNumber = "2026202015";
     string department = "Computer Science";
-
+    
+    
+    cout << "Student Profile:" << endl;
+    
     cout << "Student Name: " << name << endl;
     cout << "Roll Number: " << rollNumber << endl;
     cout << "Department: " << department << endl;
