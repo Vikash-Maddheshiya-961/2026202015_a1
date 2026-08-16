72a6d8b1e31b70150d94bd539be32cafb85ca4e4
Vikash Maddheshiya <vikash.maddheshiya@students.iiit.ac.in>
2026-08-16 17:47:09 +0530

    Created Login Branch, login.cpp file inside that and implement user login function

---
+++ login.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void login_user(){
+    string username;
+    cout << "Enter your name:";
+    cin >> username;
+    cout << "Welcome" << username <<"!" << endl;
+    return;
+}
+int main(){
+    
+    return 0;
+}
