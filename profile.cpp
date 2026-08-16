7a44fec0580f1cf5709e826f21d5a435337f32f5
Vikash Maddheshiya <vikash.maddheshiya@students.iiit.ac.in>
2026-08-16 18:09:43 +0530

    Added department to student profile!

--- profile.cpp
+++ profile.cpp
@@ -4,8 +4,11 @@ using namespace std;
 void showProfile(){
     string name = "Vikash Maddheshiya";
     string rollNumber = "2026202015";
+    string department = "Computer Science";
+
     cout << "Student Name: " << name << endl;
     cout << "Roll Number: " << rollNumber << endl;
+    cout << "Department: " << department << endl;
     return;
 }
 
