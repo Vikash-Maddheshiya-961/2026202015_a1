9f6b865a21d2e97d2b580769cbaa99486f9f1552
Vikash Maddheshiya <vikash.maddheshiya@students.iiit.ac.in>
2026-08-16 18:43:18 +0530

    Added student information module in main!

---
+++ student.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void displayStudent() {
+    string name = "Vikash Maddheshiya";
+    string rollNumber = "2026202015";
+
+    cout << "Name: " << name << '\n';
+    cout << "Roll Number: " << rollNumber << '\n';
+    
+    return;
+}
+
+int main(){
+    displayStudent();
+    return 0;
+}
